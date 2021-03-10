6d6e8055722692d36dd8e04535fdc9fed7704f52
Shyla Earl <shyla.earl@gmail.com>
2021-03-10 12:32:43 -0600

    answer 1-5 & 7 complete

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,36 +1,75 @@
 console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
-// 1. Create a variable called 'partsNeeded' with a value of the number 40 
+// 1. Create a variable called 'partsNeeded' with a value of the number 40
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
+let partsNeeded = 40;
+console.log('partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log('supplyChanges array:', supplyChanges);
 
 // 3. Console log the value of 'supplyChanges' at index 2
 console.log('3. Item at index 2 is:');
+console.log('item at index 2:', supplyChanges[2]);
 
-// 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
+// 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+let removedItem = supplyChanges.pop();
+console.log('removed last array item:', removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
+console.log('added new array value:', supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
-//    - if it is a positive number (greater than 0), log 'Added x parts.' 
+//    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, skip it. Do not log it to the console.
-//    - if the value is negative, format the log as 'Part count -x.' 
+//    - if the value is negative, format the log as 'Part count -x.'
 console.log('6. Showing supplyChanges...');
+let x = [];
+let negX = [];
+
+for(i = 0; i < supplyChanges.length; i++){
+  console.log('each value in supplyChanges:', supplyChanges[i]);
+}//end of for loop
+
+for(i = 0; i < supplyChanges.length; i++){
+  if(supplyChanges[i] > 0){
+    x.push(supplyChanges[i]);
+    // console.log('Added x parts:', x[i]);
+  } else if(supplyChanges[i] < 0){
+    negX.push(supplyChanges[i]);
+    // console.log('Part count -x', negX[i]);
+  } else if(i === 0 ){
+    //how do you not log a value?
+  }//end of else if chain
+  // console.log('Added x parts:', x[i]);
+  // console.log('Part count -x', negX[i]);
+}//end of second for loop
 
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
+for(supply of supplyChanges){
+  if(supply > 0){
+    console.log('Added x parts', supply);
+  } else if(supply < 0){
+    console.log('Part count -x', supply);
+  } else {
+    console.log('Nothing to show here!');
+  }//end of if else chain
+  // console.log('for of loop:', supply);
+}//end of for of loop
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
